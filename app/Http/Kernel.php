a9754f0937507d4db0b437f80c21b2237115946c
Sebastian Schürz <sebastian@immocheckout.com>
2024-03-07 21:30:38 +0100

    Remove Laravel Sanctum and user model – #1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,7 +30,6 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
